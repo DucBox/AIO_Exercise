2422caa4ce877fd7ac9754929c452e4223d9bf13
Ngo Quang Duc <ngoquangduc@MacBook-Air-cua-Hoang.local>
2024-07-21 20:32:45 +0700

    Xóa các file không cần thiết

--- AIO_exercise_week_1/Mean_difference_nth_root_error.py
+++
@@ -1,8 +0,0 @@
-#Calculate Mean Difference of n_th Root Error Value
-
-def md_nre_single_sample(y, yhat, n, p):
-    final_md_nre_value = pow(y, 1/n) - pow(yhat, 1/n)
-    print(f"Md_nre_single_value: {final_md_nre_value}")
-    
-if __name__ == '__main__':
-    md_nre_single_sample(y = 0.6 ,yhat = 0.1, n = 2, p = 1)
--- AIO_module_2_week_1/Image_process.py
+++
@@ -1,9 +0,0 @@
-import matplotlib . image as mpimg
-img = mpimg.imread('dog.jpeg')
-#Lightness
-# gray_img_01 = (img.max(axis=2) + img.min(axis=2)) / 2
-#Average
-# gray_img_01 = img.mean(axis = 2)
-#Luminosity
-gray_img_01 = img[:,:,0] * 0.21 + img[:,:,1] * 0.72 + img[:,:,2] * 0.07
-print(gray_img_01 [0 , 0])
